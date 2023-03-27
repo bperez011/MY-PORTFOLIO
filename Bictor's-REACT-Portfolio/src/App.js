fcd4a4f8fb5db0c43b5a9c79d7c2a8f7fd91c625
bperez011 <112590275+bperez011@users.noreply.github.com>
2023-03-27 18:09:07 -0500

    Update App.js

--- Bictor's-REACT-Portfolio/src/App.js
+++ Bictor's-REACT-Portfolio/src/App.js
@@ -3,7 +3,7 @@ import Contact from "./components/Contact";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import Skills from "./components/Skills";
-import Projects from "./components/Projects";
+/*import Projects from "./components/Projects";*/
 
 
 
@@ -13,7 +13,7 @@ function App() {
       <Navbar />
       <Home />
       <About />
-      <Projects/>
+      /*<Projects/>*/
       <Skills />
       <Contact />
     </div>
