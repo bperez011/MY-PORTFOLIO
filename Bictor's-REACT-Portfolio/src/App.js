daa1d9c041fc84732e9de11f87969aa7172d5d3b
bperez011 <112590275+bperez011@users.noreply.github.com>
2023-04-05 01:05:06 -0500

    deploy code

--- Bictor's-REACT-Portfolio/src/App.js
+++ Bictor's-REACT-Portfolio/src/App.js
@@ -2,8 +2,10 @@ import About from "./components/About";
 import Contact from "./components/Contact";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
-import Skills from "./components/Skills";
 import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+
+
 
 
 
