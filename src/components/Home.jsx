c1f278bc74b69847bbd5fb84bd8fd47905542aa4
bperez011 <112590275+bperez011@users.noreply.github.com>
2023-06-27 19:14:19 -0500

    add simple things like my smu certificate and other stuff

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -18,7 +18,7 @@ const Home = () => {
         </p>
         <div>
           <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-[#ff79c6] hover:border-[#ff79c6]'>
-            View Work
+            View Projects 
             <span className='group-hover:rotate-90 duration-300'>
               <HiArrowNarrowRight className='ml-3 ' />
             </span>
